56f339bfe9099472ce1b9046c7aad7e1c96635de
GeorgiGB <57988693+GeorgiGB@users.noreply.github.com>
2021-05-25 10:23:59 +0200

    Add files via upload
    rol_v2

---
+++ src_v2/main/java/joc/Warrior.java
@@ -0,0 +1,13 @@
+package joc;
+//puede aguantar mas daño
+public class Warrior extends Player{
+  public Warrior(String name, int attackPoints, int defensePoints, int life)
+  {
+    super(name, attackPoints, defensePoints, life);
+  }
+
+  public Warrior()
+  {
+  }
+
+}
